bd88270b1d6b518235d772437c874199e9b96eda
YasinUnall <yasinunalnn@gmail.com>
2020-05-26 19:09:53 +0300

    Added active check boxes so user can change the check box value without going into a separate page. Also addded check box listeners with ajax so check box values are saved into database when their value is changed.

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -129,6 +129,28 @@ namespace ToDoList.Controllers
             return View(toDo);
         }
 
+        [HttpPost]
+        public ActionResult AJAXEdit(int? id, bool value)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                toDo.IsDone = value;
+                db.Entry(toDo).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return PartialView("_ToDoTable", GetToDos());
+            }
+        }
+
         // GET: ToDo/Delete/5
         public ActionResult Delete(int? id)
         {
